64f608a4858dd7e3e6c67853a105d399e41b6a42
anirealtheanimezoneofficial <anirealtheanimezoneofficial@gmail.com>
2025-01-14 01:55:38 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -219,7 +219,7 @@ async def start(client, message):
             if CLONE_MODE == True:
                 buttons.append([InlineKeyboardButton('🤖 Cʀᴇᴀᴛᴇ Yᴏᴜʀ Oᴡɴ Cʟᴏɴᴇ Bᴏᴛ 🤖', callback_data='clone')])
             reply_markup = InlineKeyboardMarkup(buttons)
-            m=await message.reply_sticker("CAACAgUAAxkBAAEKVaxlCWGs1Ri6ti45xliLiUeweCnu4AACBAADwSQxMYnlHW4Ls8gQMAQ") 
+            m=await message.reply_sticker("CAACAgIAAxkBAAKWd2eFdkg14R4ld7u7bLl2DMclVYOlAAKwWgACTmAhSgsW0cE27-mQNgQ") 
             await asyncio.sleep(1)
             await m.delete()
             await message.reply_photo(
