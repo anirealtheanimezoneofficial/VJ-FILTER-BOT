6077bb91a309ffa0ef9bad7e2419210c2f49b57a
anirealtheanimezoneofficial <anirealtheanimezoneofficial@gmail.com>
2025-01-14 03:37:11 +0600

    Update info.py

--- info.py
+++ info.py
@@ -1,7 +1,3 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
 
 import re
 from os import environ
@@ -10,7 +6,7 @@ from Script import script
 id_pattern = re.compile(r'^.\d+$')
 
 # Bot information
-SESSION = environ.get('SESSION', 'TechVJBot')
+SESSION = environ.get('SESSION', 'AniReal_Anime_Zone')
 API_ID = int(environ.get('API_ID', '22660802'))
 API_HASH = environ.get('API_HASH', 'fec849af712bc23ab5f18277b1592d2e')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
@@ -21,7 +17,7 @@ PICS = (environ.get('PICS', 'https://envs.sh/vLN.jpg')).split()
 
 
 # Admins & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7273593616 6069621485').split()] # For Multiple Id Use One Space Between Each.
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()] # For Multiple Id Use One Space Between Each.
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]  # For Multiple Id Use One Space Between Each.
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 
